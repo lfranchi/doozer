44a17aecc610178bd38fbf234545496fbcb6b9cd
Leo Franchi <lfranchi@kde.org>
2012-11-09 15:56:42 +0000

    Revert "Change Walk to match filepath.Walk"
    This reverts commit 634a7322f81e2e678a8564d5f96b1833cb029c0e.

--- walk.go
+++ walk.go
@@ -6,41 +6,34 @@ import (
 
 // Walk walks the file tree in revision rev, rooted at root,
 // analogously to Walk in package path/filepath.
-func Walk(c *Conn, rev int64, root string, v WalkFunc) error {
+func Walk(c *Conn, rev int64, root string, v WalkFunc) {
 	f, err := c.Statinfo(rev, root)
 	if err != nil {
 		v(root, f, err)
-		return err
-	}
-	return walk(c, rev, root, f, v)
-}
-
-func walk(c *Conn, r int64, path string, f *FileInfo, v WalkFunc) (err error) {
-	verr := v(path, f, nil)
-	if !f.IsDir || verr == filepath.SkipDir {
 		return
 	}
-	if verr != nil {
-		return err
+	walk(c, rev, root, f, v)
+}
+
+func walk(c *Conn, r int64, path string, f *FileInfo, v WalkFunc) {
+	if !f.IsDir {
+		v(path, f, nil)
+		return
+	}
+
+	if v(path, f, nil) == filepath.SkipDir {
+		return
 	}
 
 	list, err := c.Getdirinfo(path, r, 0, -1)
 	if err != nil {
-		verr = v(path, f, err)
-		if verr != nil {
-			return err
-		}
+		v(path, f, err)
 	}
 
 	if path != "/" {
 		path += "/"
 	}
 	for i := range list {
-		err = walk(c, r, path+list[i].Name, &list[i], v)
-		if err != nil {
-			break
-		}
+		walk(c, r, path+list[i].Name, &list[i], v)
 	}
-
-	return
 }
